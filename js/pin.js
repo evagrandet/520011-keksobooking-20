4269a4c8e395d783bcfcf1e593ede23b09cdd9f0
evagrandet <evagrandet@ro.ru>
2020-07-12 18:56:32 +0300

    навела порядок в pin и utils

--- js/pin.js
+++ js/pin.js
@@ -3,10 +3,34 @@
 (function () {
 
 
-// получаю элемент, внутри которого будут располагаться все метки на карте, записываю его в переменную
-var mapPinsList = mapBlock.querySelector('.map__pins');
+  // получаю элемент, внутри которого будут располагаться все метки на карте, записываю его в переменную
+  var mapPinsList = window.map.mapBlock.querySelector('.map__pins');
 
-// получаю шаблон метки, добираюсь до разметки внутри
-var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+  // нахожу главный пин, при взаимодействии с которым происходит переход режима карты из неактивного в активный
+  var mainPin = window.map.mapBlock.querySelector('.map__pin--main');
 
+
+  // функция, которая будет срабатывать на нажатии левой кнопки мыши, и которая будет активировать карту
+  var onMainPinMousedown = function (evt) {
+    if (evt.buttons === 1) {
+      window.map.activateMap();
+    }
+  };
+
+  // функция, которая будет срабатывать на нажатии кнопки ENTER, и которая будет активировать карту
+  var onMainPinKeydown = function (evt) {
+    if (evt.key === 'Enter') {
+      window.map.activateMap();
+    }
+  };
+
+  // добавляю два обработчика событий на главный пин
+  mainPin.addEventListener('mousedown', onMainPinMousedown);
+  mainPin.addEventListener('keydown', onMainPinKeydown);
+
+
+  window.pin = {
+    mainPin: mainPin,
+    mapPinsList: mapPinsList
+  };
 })();
--- js/utils.js
+++ js/utils.js
@@ -3,23 +3,26 @@
 (function () {
 
   // функция для получения случайного элемента из массива
-var getRandomElement = function (elements) {
-  return elements[Math.floor(Math.random() * elements.length)];
-};
+  var getRandomElement = function (elements) {
+    return elements[Math.floor(Math.random() * elements.length)];
+  };
 
-
-// функция для создания массива случайной длины без повторов в элементах
-var getRandomElements = function (elements) {
-  var randomElements = [];
-  var i = 0;
-  while (i <= Math.floor(Math.random() * elements.length)) {
-    var element = elements[Math.floor(Math.random() * elements.length)];
-    if (randomElements.indexOf(element) === -1) {
-      randomElements.push(element);
-      i++;
+  // функция для создания массива случайной длины без повторов в элементах
+  var getRandomElements = function (elements) {
+    var randomElements = [];
+    var i = 0;
+    while (i <= Math.floor(Math.random() * elements.length)) {
+      var element = elements[Math.floor(Math.random() * elements.length)];
+      if (randomElements.indexOf(element) === -1) {
+        randomElements.push(element);
+        i++;
+      }
     }
-  }
-  return randomElements;
-};
+    return randomElements;
+  };
 
+  window.utils = {
+    getRandomElement: getRandomElement,
+    getRandomElements: getRandomElements
+  };
 })();
