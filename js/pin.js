eae3036de3dc26f433d89b41f4e49d4811b002b9
Eugenia Kozlova <evagrandet@ro.ru>
2020-07-27 13:43:59 +0300

    Личный проект: модуляция (часть 2) (#8)
    * wip: делаю drag and drop
    
    * выполнение задания
    
    * исправления
    
    * упс, линтер
    
    * исправления
    
    * исправление перечислений

--- js/pin.js
+++ js/pin.js
@@ -3,6 +3,17 @@
 (function () {
   var ENTER_KEY = 'Enter';
 
+  var MainPinSize = {
+    WIDTH: 62,
+    HEIGHT: 62,
+    TAIL: 25
+  };
+
+  var PinSize = {
+    WIDTH: 50,
+    HEIGHT: 70
+  };
+
   var clearPins = function () {
     var pins = window.dom.mapBlock.querySelectorAll('.map__pin:not(.map__pin--main)');
 
@@ -13,8 +24,10 @@
 
   // функция, которая будет срабатывать на нажатии левой кнопки мыши, и которая будет активировать карту
   var onMainPinMousedown = function (evt) {
-    if (evt.buttons === 1) {
+    if (evt.buttons === 1 && !window.map.isMapActivated) {
       window.map.activateMap();
+    } else if (evt.buttons === 1 && window.map.isMapActivated) {
+      window.dragAndDrop.dragAndDropMainPin(evt);
     }
   };
 
@@ -31,6 +44,8 @@
 
 
   window.pin = {
+    MainPinSize: MainPinSize,
+    PinSize: PinSize,
     onMainPinMousedown: onMainPinMousedown,
     onMainPinKeydown: onMainPinKeydown,
     clearPins: clearPins
